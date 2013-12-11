fd45c35e8613112e259d4ccb1affc13e93091d29
ngoquanghung <ngo.quang.hung@framgia.com>
2013-12-11 17:18:37 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Abcd::Application.config.secret_key_base = '484dd047161db898a42d90778a34e5219398918d7a430f75c001573842d8635bbefff0ff58398fd7654d2a12bbf1215ee84c0c8ced8ec010fb35aa43b5aaecd4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Abcd::Application.config.session_store :cookie_store, key: '_abcd_session'
